b7b37ac60795c49293ddffca99a5a6b885729de9
Niko-Jara <jaramnikol@gmail.com>
2020-06-08 17:51:44 -0600

    ActualizacionDeLab

---
+++ PriuebaRepaso/PriuebaRepaso/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriuebaRepaso
+{
+    class Persona
+    {
+        
+        private string _Nombres;
+        private string _Apellidos;
+        private char  _Genero;
+
+     
+        public string Nombres
+        {
+            get { return _Nombres; }
+            set { _Nombres = value; }
+        }
+        public string Apellidos
+        {
+            get { return _Apellidos; }
+            set { _Apellidos = value; }
+        }
+        public char Genero
+        {
+            get { return _Genero; }
+            set { _Genero = value; }
+        }
+    }
+}
--- PriuebaRepaso/PriuebaRepaso/Program.cs
+++ PriuebaRepaso/PriuebaRepaso/Program.cs
@@ -1,23 +1,21 @@
 ﻿using System;
-using System.Dynamic;
 
-namespace PracticaProgra
+namespace PriuebaRepaso
 {
     class Program
     {
-
-        private static void Operaciones()
+        private static void Operaciones() 
         {
             Console.WriteLine("Que operación matemática desea realizar 1:Suma 2:Resta 3:Dividir 4:Modulo");
             int num = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Ingrese el primer numero 1");
             int num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Ingrese el segundo numero 2");
-            int num2 = Convert.ToInt32(Console.ReadLine()); 
+            int num2 = Convert.ToInt32(Console.ReadLine());
             int suma;
             int resta;
             int dividir;
-            double  modulo;
+            double modulo;
 
             switch (num)
             {
@@ -49,40 +47,36 @@ namespace PracticaProgra
                         Console.WriteLine("Resultado del cociente es:" + modulo);
                     }
                     break;
-                default:                                         
+                default:
                     Console.WriteLine("No digitaste un número válido");
                     break;
             }
         }
-            static void Main(string[] args){
-
-            Persona objCliente;
-            
-
+        static void Main(string[] args)
+        {
             do
             {
-                objCliente = new Persona();
-                Console.WriteLine("Por favor Ingrese Nombre completo");
-                string name = Convert.ToString(Console.ReadLine());
+                Persona objpersona = new Persona();
 
-                Console.WriteLine("Por Favor Ingrese 'F' para femenino o 'M' para masculino");
-                char genero = Convert.ToChar(Console.ReadLine());
-
+                Console.WriteLine("Ingrese su Nombre");
+                objpersona.Nombres = Console.ReadLine();
+                Console.WriteLine("Ingrese sus Apellidos");
+                objpersona.Apellidos = Console.ReadLine();
+                Console.WriteLine("Ingrese su Genero Femenino 'F' y Masculino 'M'");
+                objpersona.Genero = Convert.ToChar(Console.ReadLine());
                 Program.Operaciones();
 
-                if (genero == 'F' || genero == 'f')
+                if (objpersona.Genero == 'F' || objpersona.Genero == 'f')
                 {
-                    Console.WriteLine("Que le vaya muy bien Sr." + name + genero);
+                    Console.WriteLine("Que le vaya muy bien Sr." + objpersona.Nombres);
                 }
                 else
                 {
-                    Console.WriteLine("Que le vaya muy bien Sr." + name + genero);
+                    Console.WriteLine("Que le vaya muy bien Sr." + objpersona.Nombres);
                 }
                 Console.WriteLine("-------PARA SALIR OPRIMA S--------");
 
             } while (Console.ReadKey(true).Key != ConsoleKey.S);
-
-           
         }
     }
 }
